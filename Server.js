390dc53bb3962e1b12c26e6dc08e7049c3b4d89d
Riya bisht <bisht.r2102@gmail.com>
2024-05-24 14:47:37 +0530

    chnages made

--- Server.js
+++ Server.js
@@ -1,34 +1,28 @@
-const express=require("express");
+const express = require("express");
 require("dotenv").config();
-const mongoose=require("mongoose");
-const routes=require("./view/Route");
-const CORS= require("cors")
-const bodyparser=require("body-parser")
+const mongoose = require("mongoose");
+const routes = require("./view/Route");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-
-
-
-//connection establishment
-mongoose.connect(process.env.DB_connect,{
+// Connection establishment
+mongoose.connect(process.env.DB_connect, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(()=>
-{
-    console.log("connected to database")
-}).catch((err)=>{
-    console.log("error connecting to database  :"+err.message)
-})
-const app=express();
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to database");
+}).catch((err) => {
+    console.log("Error connecting to database: " + err.message);
+});
+
+const app = express();
 app.use(express.json());
-app.use(bodyparser.json())
-app.use(CORS())
-app.use("/", routes)
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/", routes);
 
-
-
-
-//porting
-const PORT= process.env.PORT || 1001;
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
-})
+// Porting
+const PORT = process.env.PORT || 1001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
